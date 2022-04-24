371eaf80a5d4e7a7ab5ed3e00552cbc28bbc73ce
Grant Chau <40442415+gnchau@users.noreply.github.com>
2022-04-23 20:57:25 -0400

    Adding local server link for testing.

--- src/services/tuits-service.js
+++ src/services/tuits-service.js
@@ -32,4 +32,4 @@ export const updateTuit = (tid, tuit) =>
 
 export const deleteTuit = (tid) =>
     api.delete(`${TUITS_API}/${tid}`)
-        .then(response => response.data);
+        .then(response => response.data); 

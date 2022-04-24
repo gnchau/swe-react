10923818e279f5075e0e2ca02734c9b0ac5a29dc
Grant Chau <40442415+gnchau@users.noreply.github.com>
2022-04-23 21:09:10 -0400

    Adding dislike test: ensuring they display.

--- src/tests/dislikes-service.test.js
+++ src/tests/dislikes-service.test.js
@@ -11,24 +11,14 @@ test('dislike button in profile', async () => {
     const dislikes = screen.getByText(`Dislikes`);
     expect(dislikes).toBeInTheDocument();
 })
-
+ 
 describe('disliked tuits are shown in the dislike-screen', () => {
 
     beforeAll(async () => {
-        //create user
-        // const newU = await createUser(
-        //     {
-        //         username: 'test',
-        //         password: `test`,
-        //         email: `test`
-        //     }
-        // );
-        //create tuit
-        //dislike tuit
+
     })
     afterAll(async () => {
-        //delete tuit
-        //delete user
+
     })
 
     test('disliked tuits are shown in the dislike-screen', async () => {})
